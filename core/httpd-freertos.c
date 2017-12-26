8c8a6326971610c63c1ef4ed196a95eeadf23158
Chris Morgan <chmorgan@gmail.com>
2017-12-26 09:27:02 -0500

    httpd-freertos - Improve error info output when SSL_read() fails

--- core/httpd-freertos.c
+++ core/httpd-freertos.c
@@ -447,7 +447,7 @@ static void platHttpServerTask(void *pvParameters) {
 							if(ret <= 0)
 							{
 								ssl_error = SSL_get_error(pRconn->ssl, ret);
-								ESP_LOGE(TAG, "ssl_error %d", ssl_error);
+								ESP_LOGE(TAG, "ssl_error %d, ret %d, bytesStillAvailable %d", ssl_error, ret, bytesStillAvailable);
 							}
 
 							if (ret > 0) {
